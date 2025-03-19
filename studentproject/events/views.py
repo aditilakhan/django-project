c572b5c55f6fc104fe1e1e0eb42688506f166759
aditi <119800266+aditilakhan@users.noreply.github.com>
2025-03-19 03:40:33 +0000

    studentprojectmanyapp

---
+++ studentproject/events/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,HttpResponse
+
+def index(req):
+    home = f"<a href='/'>Home</a>"
+    library = f"<a href='/library/'>Library</a>"
+    exam = f"<a href='/exam/'>Exam</a>"
+    events = f"<a href='/events'>Events</a>"
+    return HttpResponse(f"<center><h1>Welcome to event app <hr>{home}\t{library}\t{exam}\t{events}</h1></center>")
+
